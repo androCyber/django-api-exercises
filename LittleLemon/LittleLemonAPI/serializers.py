9202c754c033dbbfbd939c1a97a4033a2ce8e298
Abdullah TATLICAK <tatlicak@gmail.com>
2023-01-12 10:03:40 +0300

    Serialize with function based view

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from .models import MenuItem
 
-class MenuItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=MenuItem
-        fields = ['id','title','price','inventory']
+class MenuItemSerializer(serializers.Serializer):
+   title=serializers.CharField(max_length=255)
+   price=serializers.DecimalField(max_digits=6, decimal_places=2)
+   inventory=serializers.IntegerField()

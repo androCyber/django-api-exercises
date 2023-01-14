33ec800624a624fd24a6e7033aa4b2a5dd6f0d15
Abdullah TATLICAK <tatlicak@gmail.com>
2023-01-14 10:01:39 +0300

    Token-based authentication in DRF

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path 
-from . import views 
-urlpatterns = [ 
-               path('menu-items',views.MenuItemsViewSet.as_view({'get':'list'})),
-               path('menu-items/<int:pk>',views.MenuItemsViewSet.as_view({'get':'retrieve'})),
+from django.urls import path
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('menu-items', views.menu_items),
+   path('menu-items/<int:id>', views.single_item),
+   path('category/<int:pk>',views.category_detail, name='category-detail'),
+   path('secret', views.secret),
+   path('api-token-auth',obtain_auth_token)
 ]

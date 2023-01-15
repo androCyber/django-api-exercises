6d915a34a62238d9ced8634945f242ba90f43406
Abdullah TATLICAK <tatlicak@gmail.com>
2023-01-15 07:51:53 +0300

    API throttling for class-based views

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,14 +1,6 @@
-from django.urls import path
-from . import views
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('menu-items', views.menu_items),
-   path('menu-items/<int:id>', views.single_item),
-   path('category/<int:pk>',views.category_detail, name='category-detail'),
-   path('secret/', views.secret),
-   path('api-token-auth/',obtain_auth_token),
-   path('manager-view/',views.manager_view),
-   path('throttle-check/',views.throttle_check),
-   path('throttle-check-auth/',views.throttle_check_auth),
-]
+from django.urls import path 
+from . import views 
+urlpatterns = [ 
+               path('menu-items',views.MenuItemsViewSet.as_view({'get':'list'})),
+               path('menu-items/<int:pk>',views.MenuItemsViewSet.as_view({'get':'retrieve'})),
+] 

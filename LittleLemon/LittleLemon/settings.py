ff5565334ec631191e4637b5a4ee102825e9122d
Abdullah TATLICAK <tatlicak@gmail.com>
2023-01-16 08:34:51 +0300

    Registration and authentication endpoints with JWT

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'LittleLemonAPI',
+    'djoser',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -131,16 +135,14 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework_xml.renderers.XMLRenderer',
         ],
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
-        'rest_framework.filters.SearchFilter',
-        ],
+    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
     
     'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
         ),
     'DEFAULT_THROTTLE_RATES':{
         'anon':'2/minute',
@@ -151,4 +153,14 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
 ],
+}
+
+DJOSER = {
+    "USER_ID_FIELD":"username"
+    
+
+}
+
+SIMPLE_JWT ={
+    'ACCES_TOKEN_LIFETIME': timedelta(minutes=5),
 }
--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,20 +1,99 @@
-from rest_framework.response import Response 
-from rest_framework import viewsets 
-from .models import MenuItem 
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from .models import MenuItem
 from .serializers import MenuItemSerializer
-from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from .models import Category 
+from .serializers import CategorySerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from django.core.paginator import Paginator, EmptyPage
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import UserRateThrottle
+
 from .throttles import TenCallsPerMinute
 
-class MenuItemsViewSet(viewsets.ModelViewSet):
-    #throttle_classes = [AnonRateThrottle, UserRateThrottle]
-    
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-    
-    #conditional throttling
-    def get_throttles(self):
-        if self.action == 'create':
-            throttle_classes = [UserRateThrottle]
-        else:
-            throttle_classes = [TenCallsPerMinute]
-        return [throttle() for throttle in throttle_classes]
+
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def menu_items(request):
+    if request.method == 'GET':
+        items=MenuItem.objects.select_related('category').all()
+        category_name=request.query_params.get('category')
+        to_price=request.query_params.get('to_price')
+        search=request.query_params.get('search')
+        ordering=request.query__params.get('ordering')
+        perpage=request.query__params.get('perpage',default=2)
+        page=request.query__params.get('page',default=1)
+
+        if category_name:
+            items=items.filter(category__title=category_name) #double underscore category and title
+
+        if to_price:
+            items=items.filter(price=to_price)    #price__lte means less than or equal
+
+        if search:
+            items=items.filter(title__contains=search) #icontaints and istartswith are case insentivite
+
+        if ordering:
+            ordering_fields=ordering.split(",")
+            items=items.order_by(*ordering_fields)
+
+        paginator=Paginator(items,per_page=perpage)
+        try:
+            items=paginator.page(number=page)
+        except EmptyPage:
+            items=[]
+
+        serialized_item=MenuItemSerializer(items, many=True, context={'request': request})
+        return Response(serialized_item.data)
+
+    if request.method == 'POST':
+        serialized_item=MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
+
+@api_view()
+def single_item(request,id):
+    item=get_object_or_404(MenuItem,pk=id)
+    serialized_item=MenuItemSerializer(item)
+    return Response(serialized_item.data)
+
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category,pk=pk)
+    serialized_category = CategorySerializer(category)
+    return Response(serialized_category.data) 
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message":"Some secret message"})
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def manager_view(request):
+
+    if request.user.groups.filter(name='Manager').exists():
+        return Response({"message":"Only Manager Should See This"})
+    else:
+        return Response({"message":"You are not authorized"},403)
+
+
+@api_view()
+@throttle_classes([AnonRateThrottle])
+def throttle_check(request):
+    return Response({"message":"Successful"})    
+
+@api_view()
+@permission_classes([IsAuthenticated])
+#@throttle_classes([UserRateThrottle])
+@throttle_classes([TenCallsPerMinute])
+def throttle_check_auth(request):
+    return Response({"message":"SMessage for the logged in users only"})   
